2fa6a9cbdebf1dfbde0e4b4a16edb3a9e5866cd5
Glad Chinda <gladxeqs@gmail.com>
2018-06-08 11:06:43 +0100

    Initialized index page.

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default () => (
+	<Layout>
+		<div className="container-fluid position-absolute h-100">
+			<div className="d-flex position-relative h-100 w-100 flex-wrap justify-content-center align-items-center align-content-center">
+				<h1 className="font-weight-light">Realtime Twitter-Style Poll</h1>
+			</div>
+		</div>
+	</Layout>
+);
