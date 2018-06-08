cb7936aa31d156629e96cceda53702dbfd14cdc8
Glad Chinda <gladxeqs@gmail.com>
2018-06-08 18:05:53 +0100

    Modified PollDurationAndChoices component to add duration.

--- components/NewPost.js
+++ components/NewPost.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import PollChoices from './PollChoices';
-
-const INITIAL_CHOICES = [ null, null ];
-const MIN_CHOICES_COUNT = INITIAL_CHOICES.length;
-const MAX_CHOICES_COUNT = MIN_CHOICES_COUNT + 2;
+import PollDurationAndChoices from './PollDurationAndChoices';
 
 const MAX_POST_LENGTH = 140;
 
 class NewPost extends Component {
 
-	state = { hasPoll: false, choices: INITIAL_CHOICES, post: '', postHeight: 0 }
+	state = { hasPoll: false, post: '', postHeight: 0, canSubmitPoll: false }
 
 	togglePoll = evt => {
-		let { hasPoll, choices: [ ...choices ] } = this.state;
-		hasPoll && (choices = INITIAL_CHOICES);
-		this.setState({ hasPoll: !hasPoll, choices });
+		this.setState({ hasPoll: !this.state.hasPoll, canSubmitPoll: false });
 	}
 
-	updatePostHeight() {
+	updatePostHeight = () => {
 		const postHeight = this.post.scrollHeight;
 		this.setState({ postHeight });
 	}
@@ -27,29 +21,14 @@ class NewPost extends Component {
 		(post.length <= MAX_POST_LENGTH) && this.setState({ post }, this.updatePostHeight);
 	}
 
-	updateChoice = index => evt => {
-		const choice = evt.target.value;
-		let [ ...choices ] = this.state.choices;
-
-		if (index >= 0 && index < choices.length) {
-			choices[index] = choice;
-			this.setState({ choices });
-		}
+	canSubmit = () => {
+		const { hasPoll, post, canSubmitPoll } = this.state;
+		return !post ? false : hasPoll ? canSubmitPoll : true;
 	}
 
-	addChoice = evt => {
-		const [ ...choices ] = this.state.choices;
-		choices.push(null);
-		this.setState({ choices });
-	}
-
-	removeChoice = index => evt => {
-		const [ ...currentChoices ] = this.state.choices;
-
-		if (index >= 0 && index < currentChoices.length) {
-			const choices = [ ...(currentChoices.slice(0, index)), ...(currentChoices.slice(index + 1)) ];
-			this.setState({ choices });
-		}
+	onPollUpdated = ({ duration, choices }) => {
+		const canSubmitPoll = duration && choices && (choices.filter(choice => !choice).length === 0);
+		this.setState({ canSubmitPoll });
 	}
 
 	componentDidMount() {
@@ -57,9 +36,9 @@ class NewPost extends Component {
 	}
 
 	render() {
-		const { hasPoll, post, postHeight, choices } = this.state;
+		const { hasPoll, post, postHeight } = this.state;
 
-		const title = `New ${hasPoll ? 'Poll' : 'Post'}`;
+		const type = hasPoll ? 'Poll' : 'Post';
 		const placeholder = hasPoll ? `What's the question?` : `What's happening?`;
 		const pollButtonText = hasPoll ? 'Remove Poll' : 'Add Poll';
 
@@ -67,11 +46,11 @@ class NewPost extends Component {
 			<div className="d-flex w-100 position-relative flex-wrap justify-content-center align-items-start align-content-start" style={{ maxWidth: 600 }}>
 
 				<div className="mb-5 pb-5 text-center">
-					<h1 className="h2 font-weight-bold mb-3 text-dark">{title}</h1>
+					<h1 className="h2 font-weight-bold mb-3 text-dark">{ `New ${type}` }</h1>
 					<button className="btn btn-outline-info py-0 text-uppercase font-weight-bold" onClick={ this.togglePoll } style={{ height: 36, borderRadius: 18, borderWidth: 2, fontSize: '0.75rem', lineHeight: 1, width: 120 }}>{ pollButtonText }</button>
 				</div>
 
-				<div className="w-100">
+				<div className="w-100 mb-3">
 					<textarea ref={e => this.post = e} className="border-bottom border-gray w-100 h3 px-3 py-2 font-weight-normal" placeholder={placeholder} value={post} rows="1" onChange={this.handlePostUpdate} style={{ resize: 'none', outline: 'none', boxShadow: 'none', border: 'none', lineHeight: 1.4, overflow: 'hidden', height: postHeight }} />
 
 					<div className="w-100 text-right text-secondary">
@@ -79,7 +58,11 @@ class NewPost extends Component {
 					</div>
 				</div>
 
-				{ hasPoll && <PollChoices /> }
+				{ hasPoll && <PollDurationAndChoices onPollUpdated={this.onPollUpdated} /> }
+
+				{ this.canSubmit() && <button className="btn btn-info font-weight-bold text-uppercase mt-5" style={{ height: 48, borderRadius: 24, lineHeight: 1, width: 160 }} onClick={this.updateDuration}>
+					{ `Create ${type}` }
+				</button> }
 
 			</div>
 		);
---
+++ components/PollDurationAndChoices.js
@@ -0,0 +1,95 @@
+import moment from 'moment';
+import React, { Component } from 'react';
+
+const INITIAL_CHOICES = [null, null];
+const MIN_CHOICES_COUNT = INITIAL_CHOICES.length;
+const MAX_CHOICES_COUNT = MIN_CHOICES_COUNT + 2;
+
+const POLL_DURATIONS = [60, 30, 15, 5, 1];
+
+class PollDurationAndChoices extends Component {
+
+	state = { choices: INITIAL_CHOICES, duration: POLL_DURATIONS[0] }
+
+	addChoice = evt => {
+		const [ ...choices ] = this.state.choices;
+		choices.push(null);
+		this.setState({ choices }, this.afterPollUpdated);
+	}
+
+	removeChoice = index => evt => {
+		const [ ...currentChoices ] = this.state.choices;
+
+		if (index >= 0 && index < currentChoices.length) {
+			const choices = [
+				...(currentChoices.slice(0, index)),
+				...(currentChoices.slice(index + 1))
+			];
+
+			this.setState({ choices }, this.afterPollUpdated);
+		}
+	}
+
+	updateChoice = index => evt => {
+		const choice = evt.target.value;
+		let [ ...choices ] = this.state.choices;
+
+		if (index >= 0 && index < choices.length) {
+			choices[index] = choice;
+			this.setState({ choices }, this.afterPollUpdated);
+		}
+	}
+
+	updateDuration = duration => evt => {
+		this.setState({ duration: Math.max(1, Math.min(60, +duration)) }, this.afterPollUpdated);
+	}
+
+	afterPollUpdated = () => {
+		const { ...currentState } = this.state;
+		const { onPollUpdated = f => f } = this.props;
+		onPollUpdated(currentState);
+	}
+
+	render() {
+		const { choices } = this.state;
+		const canAddChoices = choices.length < MAX_CHOICES_COUNT;
+
+		return (
+			<div className="d-flex flex-wrap justify-content-start w-100">
+
+				<div className="mb-5 w-100">
+
+					{ choices.map((choice, index) => (
+						<div key={index} className="w-100 py-2 d-flex justify-content-start">
+
+							<input type="text" className="w-50 form-control position-relative" value={choice || ''} maxLength="50" placeholder={`Choice ${index + 1}`} onChange={this.updateChoice(index)} />
+
+							{ (index >= MIN_CHOICES_COUNT) && <button className="btn btn-link text-uppercase font-weight-bold ml-3 px-0" onClick={this.removeChoice(index)} style={{ fontSize: '0.7rem', textDecoration: 'none' }}>Remove</button> }
+
+						</div>
+					)) }
+
+					{ canAddChoices && <button className="btn btn-link text-uppercase font-weight-bold mt-4 px-0" onClick={this.addChoice} style={{ fontSize: '0.8rem', textDecoration: 'none' }}>+ Add Choice</button> }
+
+				</div>
+
+				<div className="w-100 border-top border-bottom py-4 border-gray d-flex justify-content-between align-items-center">
+					<span className="font-weight-bold text-uppercase text-secondary mx-2">Duration</span>
+					<div className="mx-2">
+						{ POLL_DURATIONS.map((duration, index) => {
+							const currentDuration = duration === this.state.duration;
+
+							return <button key={index} className={`btn mr-2 font-weight-bold ${currentDuration ? 'btn-primary' : 'btn-secondary'}`} disabled={currentDuration} style={{ height: 36, borderRadius: 18, fontSize: '0.75rem', lineHeight: 1, width: 75 }} onClick={this.updateDuration(duration)}>
+								{ duration % 60 ? `${duration} ${duration > 1 ? 'mins' : 'min'}` : `${duration / 60} hour` }
+							</button>
+						}) }
+					</div>
+				</div>
+
+			</div>
+		)
+	}
+
+}
+
+export default PollDurationAndChoices;
