bdfad369c7abcadd39ed7013a45e856c6270deeb
Glad Chinda <gladxeqs@gmail.com>
2018-06-11 12:50:56 +0100

    Added Loader component to pages and other components.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import '../styles/_global.scss';
 import axios from 'axios';
 import Link from 'next/link';
 import Pusher from 'pusher-js';
@@ -5,12 +6,12 @@ import React, { Component, Fragment } from 'react';
 import * as Session from '../helpers/session';
 
 import Post from '../components/Post';
-import Layout from '../components/Layout';
+import Loader from '../components/Loader';
 import ChoosePersona from '../components/ChoosePersona';
 
 class IndexPage extends Component {
 
-	state = { user: null, people: [], posts: [] }
+	state = { user: null, people: [], posts: [], loading: true }
 
 	personaSelected = user => this.setState({ user }, () => Session.initializeSession(user))
 
@@ -34,15 +35,19 @@ class IndexPage extends Component {
 
 		this.pusher.connection.bind('connected', () => {
 
-			axios.get('/api/users').then(({ data }) => {
-				const { users: people = [] } = data;
-				this.setState({ people });
-			});
+			this.setState({ loading: true });
 
-			axios.get('/api/posts').then(({ data }) => {
-				const { posts = [] } = data;
-				this.setState({ posts });
-			});
+			Promise.all([
+				axios.get('/api/users').then(({ data }) => {
+					const { users: people = [] } = data;
+					this.setState({ people });
+				}),
+
+				axios.get('/api/posts').then(({ data }) => {
+					const { posts = [] } = data;
+					this.setState({ posts });
+				})
+			]).then(() => { this.setState({ loading: false }) });
 
 		});
 	}
@@ -53,12 +58,14 @@ class IndexPage extends Component {
 
 	render() {
 
-		const { user = null, posts, people } = this.state;
+		const { user = null, posts, people, loading } = this.state;
 		let activeUser = null;
 
 		if (people && user) {
 			activeUser = people.find(person => person.id === user);
 		}
+
+		if (loading) return <Loader />
 
 		return <Fragment>
 			{ activeUser
@@ -66,7 +73,7 @@ class IndexPage extends Component {
 				? ( posts.length > 0
 
 					? <div className="bg-dark d-flex w-100 flex-wrap justify-content-center align-items-start" style={{ minHeight: '100vh' }}>
-						<div className="bg-dark position-fixed py-5 d-flex align-items-center justify-content-between" style={{ left: 30, right: 30,  zIndex: 100 }}>
+						<div className="bg-dark position-fixed py-5 d-flex align-items-center justify-content-between" style={{ left: 30, right: 30, zIndex: 100 }}>
 							<Link prefetch replace passHref href="/post">
 								<button className="btn btn-info text-uppercase font-weight-bold" style={{ height: 40, borderRadius: 20, fontSize: '0.8rem', lineHeight: 1, width: 120 }}>New Post</button>
 							</Link>
@@ -93,10 +100,10 @@ class IndexPage extends Component {
 
 					: <Fragment>
 							<div className="position-absolute w-100 h-100 px-3 pb-5 d-flex flex-wrap align-items-center align-content-center justify-content-center" style={{ top: 0, bottom: 0 }}>
-								<h1 className="font-weight-light w-100 text-center mb-3">Realtime Twitter-Style Poll</h1>
+								<h1 className="font-weight-light w-100 text-center mb-3">Realtime Poll</h1>
 
 								<Link prefetch replace passHref href="/post">
-									<button className="btn btn-link text-uppercase font-weight-bold ml-3 px-0" style={{ textDecoration: 'none', fontSize: '0.9rem' }}>New Post</button>
+									<button className="btn btn-info text-uppercase font-weight-bold" style={{ height: 40, borderRadius: 20, fontSize: '0.8rem', lineHeight: 1, width: 120 }}>New Post</button>
 								</Link>
 							</div>
 					</Fragment>
@@ -109,11 +116,9 @@ class IndexPage extends Component {
 }
 
 export default () => (
-	<Layout>
-		<div className="container-fluid px-0">
-			<div className="d-flex w-100 flex-wrap justify-content-center align-items-center align-content-center">
-				<IndexPage />
-			</div>
+	<div className="container-fluid px-0">
+		<div className="d-flex w-100 flex-wrap justify-content-center align-items-center align-content-center">
+			<IndexPage />
 		</div>
-	</Layout>
+	</div>
 );
--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,7 @@
-import Link from 'next/link';
+import '../styles/_global.scss';
 import Router from 'next/router';
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import * as Session from '../helpers/session';
-import Layout from '../components/Layout';
 import NewPost from '../components/NewPost';
 
 class NewPostPage extends Component {
@@ -17,27 +16,13 @@ class NewPostPage extends Component {
 	}
 
 	render() {
-		return this.state.user && (
-			<Fragment>
-				<div className="position-absolute bg-white mt-5" style={{ left: 30, right: 0, zIndex: 100 }}>
-					<Link prefetch replace passHref href="/">
-						<button className="btn btn-link text-uppercase font-weight-bold ml-3 px-0" style={{ textDecoration: 'none', fontSize: '0.9rem' }}>Back to Posts</button>
-					</Link>
-				</div>
-
-				<div className="d-flex position-relative h-100 w-50 mx-auto flex-wrap justify-content-center align-items-center align-content-center">
-					<NewPost user={this.state.user} />
-				</div>
-			</Fragment>
-		);
+		return this.state.user && <NewPost user={this.state.user} />
 	}
 
 }
 
 export default () => (
-	<Layout>
-		<div className="container-fluid position-absolute h-100">
-			<NewPostPage />
-		</div>
-	</Layout>
+	<div className="container-fluid position-absolute h-100">
+		<NewPostPage />
+	</div>
 );
